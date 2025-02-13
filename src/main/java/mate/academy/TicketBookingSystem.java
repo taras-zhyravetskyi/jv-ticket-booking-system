472088d60ebe168956f1dfd66dd475d74bcf1265
Taras Zhyravetskyi <tarik.zhyravetskyy@gmail.com>
2025-02-13 21:44:42 +0200

    Revert " Task completed"
    This reverts commit 294dc0532c5bd52aed3ac99fc2b89da35ef90190.

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,27 +1,12 @@
 package mate.academy;
 
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class TicketBookingSystem {
-    private Semaphore semaphore;
-    private AtomicInteger counter;
 
     public TicketBookingSystem(int totalSeats) {
-       semaphore = new Semaphore(totalSeats);
-       counter = new AtomicInteger(totalSeats);
+
     }
 
     public BookingResult attemptBooking(String user) {
-        BookingResult bookingResult;
-        try {
-            semaphore.tryAcquire();
-            bookingResult = new BookingResult(user, true, "Booking successful.");
-            semaphore.release(counter.decrementAndGet());
-        } catch (RuntimeException e) {
-            return new BookingResult (user, false, "No seats available.");
-        }
-        return bookingResult;
+        return null;
     }
 }
